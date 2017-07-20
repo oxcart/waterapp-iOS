a0e107cb04ab71cf045c1c8c4a16440d6af15fa8
cactis <cactis.lin@gmail.com>
2017-07-20 15:38:39 +0800

    Initial Commit

---
+++ waterapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  waterapp
+//
+//  Created by ctslin on 20/07/2017.
+//  Copyright © 2017 airfont.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
