a57a108b71355b8f369beabb25ce305f0da7e41a
cactis <cactis.lin@gmail.com>
2017-07-20 23:11:13 +0800

    首頁

---
+++ waterapp/Configure.swift
@@ -0,0 +1,42 @@
+//
+//  Configure.swift
+//  waterapp
+//
+//  Created by ctslin on 20/07/2017.
+//  Copyright © 2017 airfont.com. All rights reserved.
+//
+
+import Foundation
+import SwiftEasyKit
+
+class Configure {
+  init() {
+    K.App.mode = "produciton"
+    K.App.mode = "local"
+    
+    K.App.name = "Waterapp"
+    
+    K.Api.production = "http://172.104.79.212/"
+    K.Api.local = "http://waterapp.dev/"
+    
+    
+    switch K.App.mode {
+    case "production":
+      K.Api.host = K.Api.production + K.Api.prefix
+    case "stage":
+      K.Api.host = K.Api.stage + K.Api.prefix
+    default:
+      K.Api.host = K.Api.local + K.Api.prefix
+    }
+  }
+}
+
+
+extension K {
+  struct Api {
+    struct Resource {
+      static var categories = "/categories"
+    }
+    
+  }
+}
